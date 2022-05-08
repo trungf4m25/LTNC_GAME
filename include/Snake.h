131999b5b27f18c4f1f795dc16aac2ef0a157037
trungf4m25 <100523058+trungf4m25@users.noreply.github.com>
2022-05-08 20:52:00 +0700

    Update Snake.h

--- include/Snake.h
+++ include/Snake.h
@@ -2,7 +2,7 @@
 #define SNAKE_H
 
 #include <windows.h>
-#include <vector>
+#include <deque>
 
 #define WIDTH 50
 #define HEIGHT 25
@@ -16,13 +16,13 @@ class Snake
         int vel;
         char dir;
         int len;
-        vector<COORD> body;
+        deque<COORD> body;
     public:
         Snake(COORD pos, int vel);
         void grow();
         void moveSnake();
         void direction(char dir);
-        vector<COORD> getBody();
+        deque<COORD> getBody();
         bool collided();
         bool eaten(COORD food);
         COORD getPos();
